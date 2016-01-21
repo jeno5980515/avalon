3cd3b41e5419fc404061dc90d0f3621e6bac6db4
jeno5980515 <jeno5980515@yahoo.com.tw>
2016-01-22 00:02:11 +0800

    f

--- index.js
+++ index.js
@@ -2,13 +2,8 @@
 	var express = require('express');
 	var app = express();
 	var server = require('http').createServer(app);
-	var io = require('socket.io')(server);
-	var port = 8080 ;
-
-	server.listen(port, function () {
-	  console.log('Server listening at port %d', port);
-	});
-
+	var io = require('socket.io').listen(server);
+	server.listen(process.env.PORT || 8080);
 	app.use(express.static(__dirname ));
 
 
