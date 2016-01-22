3b80bc6f7404567bdbbeea30f1d82e9c8fdbb626
jeno5980515 <jeno5980515@yahoo.com.tw>
2016-01-22 23:41:29 +0800

    recover function

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 	var app = express();
 	var server = require('http').createServer(app);
 	var io = require('socket.io').listen(server);
-	//server.listen(process.env.PORT || 8080);
-	server.listen(8080);
+	server.listen(process.env.PORT || 8080);
+	//server.listen(8080);
 	app.use(express.static(__dirname ));
 
 
