1d1be9b6d43d1f2a3134a6568634055daf16a7ea
jeno5980515 <jeno5980515@yahoo.com.tw>
2016-09-28 20:54:30 +0800

    test

--- js/client.js
+++ js/client.js
@@ -1,5 +1,6 @@
 (function(){
-	var socket = io.connect('http://my-avalon.herokuapp.com/');
+	var socket = io.connect('http://avalon-test.herokuapp.com/');
+	//var socket = io.connect('http://my-avalon.herokuapp.com/');
 	//var socket = io.connect('localhost:8080');
 	var gb = null 
 	var roomNumber = null ;
@@ -1030,6 +1031,7 @@
 
 	//document.addEventListener('visibilitychange', visibleChangeHandler, false);
 	var notification = window.Notification || window.mozNotification || window.webkitNotification;
+	alert(notification);
 	notification.requestPermission(function(permission){});
 
 	var originalTitle = '', messageCount = 0;
