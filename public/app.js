fc59b1150a63f5c24a5d8fe7676ed9b7e50c8b3d
nchang <neil.chang@innovasolutions.com>
2017-07-07 16:08:43 +0800

    remove unecessary files

--- public/app.js
+++
@@ -1,8 +0,0 @@
-(function(){
-	var express = require('express');
-	var app = express();
-
-	app.use('/avalon', require('./avalon'));
-	var port = Number(process.env.PORT || 8080);
-	app.listen(port);
-})();
